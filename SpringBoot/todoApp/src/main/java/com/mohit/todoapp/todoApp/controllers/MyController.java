be17f9ba46ff091cfc932182acdfd379a13b0011
Mohit <mohitsingh.it5@gmail.com>
2022-09-21 21:07:09 +0530

    Setting up jsp view pages | Mohit

---
+++ SpringBoot/todoApp/src/main/java/com/mohit/todoapp/todoApp/controllers/MyController.java
@@ -0,0 +1,23 @@
+package com.mohit.todoapp.todoApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+
+	@GetMapping("/say-hello")
+//	@RequestMapping("/hello") // requestMapping has more priority
+	@ResponseBody
+	public String sayHello() {
+		return "Hello, What's on your mind today?";
+	}
+	
+	
+	@GetMapping("/hello")
+	public String sayHelloView() {
+		return "hello";
+	}
+}
