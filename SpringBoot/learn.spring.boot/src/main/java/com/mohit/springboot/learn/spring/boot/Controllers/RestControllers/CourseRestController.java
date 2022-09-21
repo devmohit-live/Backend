b1b6c777fd0f50caed930d8cf78f1f5703c74b4e
Mohit <mohitsingh.it5@gmail.com>
2022-09-21 19:09:51 +0530

    Added proper commneted msgs

--- SpringBoot/learn.spring.boot/src/main/java/com/mohit/springboot/learn/spring/boot/Controllers/RestControllers/CourseRestController.java
+++ SpringBoot/learn.spring.boot/src/main/java/com/mohit/springboot/learn/spring/boot/Controllers/RestControllers/CourseRestController.java
@@ -11,12 +11,16 @@ import com.mohit.springboot.learn.spring.boot.Entity.Course;
 @RestController
 public class CourseRestController {
 
+	//Jackson : automatically converts list of courses to json
+	// whitelabel page is automatically setted-up by ErrorMvcAutoConfiuration
+//	 Spring boot :  Dependency injection(spring) and AutoConfiguration (ErrorMvcAutoConfiuration is one of the autoconfiguration sone by boot)
+	
 	@GetMapping(path = "/courses")
 	public List<Course> retrieveAllCourses(){
 		return List.of(
 				new Course(1,"Learn Java","Mohit"),
-				new Course(1,"Learn Python","Mohit"),
-				new Course(1,"Learn DevOps","Mohit")
+				new Course(2,"Learn Python","Mohit"),
+				new Course(3,"Learn DevOps","Mohit")
 				);
 	}
 }
