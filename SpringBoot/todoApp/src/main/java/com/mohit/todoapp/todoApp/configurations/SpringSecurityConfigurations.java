06033de99aa710e2e4ddd71f9c7c1973b349ea7b
Mohit <mohitsingh.it5@gmail.com>
2022-09-28 20:08:35 +0530

    Created Password Encoder for UserDetails

--- SpringBoot/todoApp/src/main/java/com/mohit/todoapp/todoApp/configurations/SpringSecurityConfigurations.java
+++ SpringBoot/todoApp/src/main/java/com/mohit/todoapp/todoApp/configurations/SpringSecurityConfigurations.java
@@ -1,20 +1,35 @@
 package com.mohit.todoapp.todoApp.configurations;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfigurations {
 //generally LDAP or Db is used for user details
-	//we are here using inMewmory User details
-	
+	// we are here using inMewmory User details
+
 	@Bean
 	public InMemoryUserDetailsManager createUserDetails() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder().username("mohit").password("123")
-		.roles("USER","ADMIN").build();
+		
+		// function to use Bycrypt encoder and incode password
+		Function<String, String> passwordEncoder = inp->passwordEncoder().encode(inp);
+		
+		UserDetails userDetails = User.builder()
+				.passwordEncoder(passwordEncoder )
+				.username("mohit").password("123")
+				.roles("USER", "ADMIN").build();
 		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
