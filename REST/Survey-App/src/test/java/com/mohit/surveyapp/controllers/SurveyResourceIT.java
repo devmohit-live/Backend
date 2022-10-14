91490e327bb6d1d3e159abc608f39ec1109f361a
Mohit <mohitsingh.it5@gmail.com>
2022-10-14 18:38:11 +0530

    Tested Get Methods | Mohit

--- REST/Survey-App/src/test/java/com/mohit/surveyapp/controllers/SurveyResourceIT.java
+++ REST/Survey-App/src/test/java/com/mohit/surveyapp/controllers/SurveyResourceIT.java
@@ -1,9 +1,17 @@
 package com.mohit.surveyapp.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -16,37 +24,74 @@ import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SurveyResourceIT {
-	String expectedResponse = """
-			{
-					"id": "Question1",
-					"description": "Most Popular Cloud Platform Today",
-					"options": [
-					"AWS",
-					"Azure",
-					"Google Cloud",
-					"Oracle Cloud"
-					],
-					"correctAnswer": "AWS"
-				}
-			""";
 
 	@Autowired
 	private TestRestTemplate template;
 	private final String SPECIFIC_QUESTION_URL = "/surveys/Survey1/questions/Question1";
+	private final String GENERIC_QUESTION_URL = "/surveys/Survey1/questions/";
 
 	@Test
-	void getSurveyQuestionsById_basicScenario() {
+	void getSurveyQuestionsById_basicScenario() throws JSONException {
 
 		ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
-		System.out.println(responseEntity.getBody());
-		System.out.println(responseEntity.getHeaders());
-		String expectedResponse = """ 
-				{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+
+		String expectedResponse = """
+				{
+						"id": "Question1",
+						"description": "Most Popular Cloud Platform Today",
+						"options": [
+						"AWS",
+						"Azure",
+						"Google Cloud",
+						"Oracle Cloud"
+						],
+						"correctAnswer": "AWS"
+					}
 				""";
-		assertEquals(expectedResponse.trim(), responseEntity.getBody()); //not recommeded
-		
-		
-//		[Content-Type:"application/json", Transfer-Encoding:"chunked", Date:"Fri, 14 Oct 2022 07:54:06 GMT", Keep-Alive:"timeout=60", Connection:"keep-alive"]
-		
+
+		// check for status code
+//		assertEquals(200, responseEntity.getStatusCodeValue());
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		// check for headers : Content-Type:"application/json",
+		// header returns a list
+		assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+		// check body
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
+
+	}
+
+	@Test
+	void geSurveyQuestions_basicTest() throws JSONException {
+		var expectedResponse = """
+						[
+							{
+								"id": "Question1",
+								"description": "Most Popular Cloud Platform Today",
+								"correctAnswer": "AWS"
+							},
+							{
+								"id": "Question2",
+								"description": "Fastest Growing Cloud Platform",
+								"correctAnswer": "Google Cloud"
+							},
+							{
+								"id": "Question3",
+								"description": "Most Popular DevOps Tool",
+								"correctAnswer": "Kubernetes"
+							}
+							]
+				""";
+		ResponseEntity<String> responseEntity = template.getForEntity(GENERIC_QUESTION_URL, String.class);
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+		assertEquals("application/json", responseEntity.getHeaders().get("Content-Type").get(0));
+		System.out.println("Output is "+ responseEntity.getBody().toString());
+//		  [
+	//		{id=Question1, description=Most Popular Cloud Platform Today, options=[AWS, Azure, Google Cloud, Oracle Cloud], correctAnswer=AWS}, 
+	//		{id=Question2, description=Fastest Growing Cloud Platform, options=[AWS, Azure, Google Cloud, Oracle Cloud], correctAnswer=Google Cloud}, 
+	//		{id=Question3, description=Most Popular DevOps Tool, options=[Kubernetes, Docker, Terraform, Azure DevOps], correctAnswer=Kubernetes}
+//		]
+
+		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody().toString(), false);
+
 	}
 }
