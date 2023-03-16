35313bb75a3a13c858287caea9ab99c275acd291
Mohit Singh <mohitsingh.it5@gmail.com>
2023-03-17 02:58:28 +0530

    Custom Bean Creation, Config, Context

---
+++ Spring basics/spring-basics-01/src/main/java/com/springbasics/springbasics01/SpringBasics01Application.java
@@ -0,0 +1,13 @@
+package com.springbasics.springbasics01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasics01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasics01Application.class, args);
+	}
+
+}
---
+++ Spring basics/spring-basics-01/src/test/java/com/springbasics/springbasics01/SpringBasics01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbasics.springbasics01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasics01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
